4e624d1d11dbc47151b20d93cb0c8cbc768ea9d6
TilloTenney <131945703+TilloTenney@users.noreply.github.com>
2023-05-02 14:56:45 +0530

    Day_1_Project_Band_Name_Generator_02052023

---
+++ Day_1_Project_Band_Name_Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to Brand Name Generator\n")
+
+City_Name = input("Where are you from?\n")
+
+Pet_Name = input("What's your pet name?\n")
+
+print("Your brand name could be " + City_Name + " " + Pet_Name)
